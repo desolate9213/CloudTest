29d819e971bcd981caaea29b622e7217c37309ce
desolate9213 <47103624+desolate9213@users.noreply.github.com>
2021-04-07 14:47:28 +0700

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 app = Flask (__name__)
-
-
-@server.route("/")
-def hello():
-    return "hello Toei"
+@app.route('/')
+def Home():
+    return render_template("home.html")
+@app.route("/aboutUs")
+def aboutUs():
+    return render_template("aboutUs.html")
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0')
    # app.run(debug = True)
