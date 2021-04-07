ec698d419317793d3e77934bf40338033d236ea9
desolate9213 <47103624+desolate9213@users.noreply.github.com>
2021-04-07 15:41:48 +0700

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask (__name__)
 @app.route('/')
 def Home():
-    return "6006021610040"
+    return "6006021610040 ธงชัย หนำเหรี๊ยบ"
 
 if __name__ == '__main__':
     app.debug = True
